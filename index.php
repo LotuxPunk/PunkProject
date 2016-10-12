9cd5961df5c72fc2b67feb66b28d7c56b50537c6
LotuxPunk <clement@vandendaelen.com>
2016-10-12 02:42:24 +0200

    Bug hunt

--- index.php
+++ index.php
@@ -81,8 +81,6 @@
 	<meta charset="utf-8">
 	<title>PunkVote - Votez pour ImperAttack</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-
 	<!-- UN PEU DE CSS POUR ADAPTER BOOTSTRAP A NOS BESOINS -->
 	<style type="text/css">
 		body{
