4f900de1434972d91121b674913f092047cc71c1
DorinnB <dorinn@netcourrier.com>
2016-11-13 15:41:46 +0100

    nouvelle fonction connection de l'autre branch
    creation de la fonction de connection.
    je l'utilise

--- gallery/newimg.php
+++ gallery/newimg.php
@@ -4,18 +4,10 @@ if (!isset($_POST['id']) OR !isset($_POST['next'])) {
 }
 ?>
 
+
 <?php
-	$serveur="localhost";
-	$login = "imperacu_vote";
-	$pass = "ilfautvoter15";
-	
-	try{
-		$bdd = new PDO("mysql:host=$serveur;dbname=imperacu_vote;charset=utf8",$login,$pass);
-	}
-	catch (Exception $e)
-	{
-		die('Erreur : ' . $e->getMessage());
-	}
+	require "functions.php"
+	connexionDB();
 ?>
 
 <?php
